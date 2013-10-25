bc4d2d4b9aa93da0c9c66a6a53aa721816db1031
oyster.ouyang <oyster.ouyang@gmail.com>
2013-10-25 17:41:32 +0800

    new demo site

---
+++ Demo/DemoSite/Remark.cshtml.cs
@@ -0,0 +1,56 @@
+﻿
+namespace demosite
+{
+    using oyster.web;
+    using oyster.web.define;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web;
+
+    public class Remark : TemplateBase<Remark>
+    {
+        static Remark()
+        {
+            templateSections.Add("Page",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"<h1>
+    神奇的Visual Studio,站点根目录下必须有个.cshtml的文件，否则子目录的.cshtml会映射不了编译绑定。 虽然我们不用他的绑定编译，但编辑页面时看着警告，确实不爽啊。
+</h1>
+");});
+
+        }
+
+        
+        public override object[] Init(Request request)
+        {
+            
+            
+    return new object[0];
+
+        }
+        
+        public void RequestInternal(Response response)
+        {
+            
+
+
+        }
+
+        public static object[] Parameters()
+        {
+            return new object[] { };
+        }
+        public override void Request(Request request,Response response)
+        {
+            object[] parms=request.Body.Paramters;
+            if(parms==null)
+                throw new Exception("Paramters no set!");
+            
+            if(parms.Length==0)
+                RequestInternal(response);        
+        }
+    }
+}
---
+++ Demo/DemoSite/_Home.cshtml.cs
@@ -0,0 +1,103 @@
+﻿
+namespace demosite
+{
+    using oyster.web;
+    using oyster.web.define;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web;
+
+    public class _home : TemplateBase<_home>
+    {
+        static _home()
+        {
+            templateSections.Add("Page",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"" />
+    <meta name=""description"" content=""");
+            Echo(html, Model.Head.description);
+            Echo(html, @""" />
+    <meta name=""keywords"" content=""");
+            Echo(html, Model.Head.keywords);
+            Echo(html, @""" />
+    <title>");
+            Echo(html, Model.Head.title);
+            Echo(html, @"</title>
+    ");
+            invorker.Invoke(typeof(_home),"Head");
+            Echo(html, @"
+</head>
+<body>
+    ");
+            invorker.Invoke(typeof(_home),"Body");
+            invorker.Invoke(typeof(_home),"Foot");
+            Echo(html, @"
+</body>
+</html>
+");
+            invorker.Invoke(typeof(_home),"OutHtml");});
+templateSections.Add("Head",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"
+        <!--html head-->
+   ");});
+templateSections.Add("Body",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"
+        <!--html body-->
+   ");});
+templateSections.Add("Foot",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"
+        <!--html foot-->
+   ");});
+templateSections.Add("OutHtml",(html,response,invorker)=>{
+    dynamic Model=response.Model;
+
+            Echo(html, @"
+    <!--out html-->
+");});
+
+        }
+
+        
+        public override object[] Init(Request request)
+        {
+            
+            
+    return new object[0];
+
+        }
+        
+        public void RequestInternal(Response response)
+        {
+            
+
+
+        }
+
+        public static object[] Parameters()
+        {
+            return new object[] { };
+        }
+        public override void Request(Request request,Response response)
+        {
+            object[] parms=request.Body.Paramters;
+            if(parms==null)
+                throw new Exception("Paramters no set!");
+            
+            if(parms.Length==0)
+                RequestInternal(response);        
+        }
+    }
+}
